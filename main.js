caf374c505c0e721aaed45c85a3b882885f51f3a
laya-star <78084935+laya-star@users.noreply.github.com>
2021-09-05 17:21:57 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function page(){
+    window.location = "alphabet.html";
+}
+
+function page1(){
+    window.location = "animal.html";
+}
+
+function page2(){
+    window.location = "word.html";
+}
